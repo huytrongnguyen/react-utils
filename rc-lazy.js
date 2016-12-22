e1374a76184105f153c46ac609b09d3d539f5e90
huynguyen <huytrongnguyen@kms-technology.com>
2016-12-22 13:41:38 +0700

    Update rc-lazy.js

---
+++ rc-lazy.js
@@ -0,0 +1,9 @@
+/**
+ * index.js
+ *
+ * This is the entry file for the application, only setup and boilerplate code.
+ */
+
+export { MutationType } from './data/store'
+export { default as Store } from './data/store'
+export { default as LazyContainer } from './components/lazy'
